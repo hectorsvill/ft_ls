8d2b4cb955ca8d211f0b1c032857c1d6c9b6ae4e
l <hvilasa>
2017-04-05 17:33:09 -0700

    make

--- Makefile
+++ Makefile
@@ -1,13 +1,17 @@
 
+NAME = ft_ls
+
 LIB = libft/libft.a
 
 CC = gcc -Wall -Wextra -Werror
 
 SRC = main.c
 
-all: $(SRC)
+all: $(NAME) 
+
+$(NAME): $(SRC) 
 	@make -C libft
-	@$(CC) $(SRC) -Iinc $(LIB) 
+	@$(CC) $(SRC) -Iinc $(LIB) -o $(NAME) 
 
 clean:
 	@make clean -C libft
--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "libft/inc/ft_printf.h"
 #include <sys/types.h>
 #include <dirent.h>
+
 /*
 **  List files in current directory:
 */
+/*
 int main(void)
 {
     struct dirent *dp;
@@ -14,6 +16,25 @@ int main(void)
             ft_printf("%s\n", dp->d_name);
         closedir(dfd);
     }
+    return (0);
+}
+*/
+
+int main(void)
+{
+    DIR *dir;
+    struct dirent *sd;
+
+    dir = opendir(".");
+
+    if (!dir)
+    {
+        ft_printf("Error: Unable to open directory");
+        return (0);
+    }
+    while ((sd = readdir(dir)) != NULL)
+        ft_printf("%s\n", sd->d_name);
+    closedir(dir);
     return (0);
 }
 
