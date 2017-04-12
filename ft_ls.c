c50d0d1be395b3280bd65d4cce8961e8823466f7
hsv <hectorstevenvillasano@outlook.com>
2017-04-12 10:54:41 -0700

    will need to create new link list with more members

--- ft_ls.c
+++ ft_ls.c
@@ -7,7 +7,19 @@
 
 */
 
-void ls_openprintdir(t_list *name_list, char *name)
+void print_list(t_list *list)
+{
+	t_list *tmp;
+
+	tmp = list;
+	while (list)
+	{
+		ft_putendl(tmp->content);
+		tmp = list->next;
+	}
+}
+
+void ls_openprintdir(t_list **name_list, char *name)
 {
     /**
     **  This fx writes a list of the directory name
@@ -22,19 +34,19 @@ void ls_openprintdir(t_list *name_list, char *name)
 		ft_printf("ls: cannot access %s: No such file or directory\n", name);
 
 	tmp = ft_lstnew("", 0);
-	name_list = ft_lstnew("", 0);
+	*name_list = ft_lstnew("", 0);
 	while ((sd = readdir(dir)) != NULL)
 		if(ft_strncmp(sd->d_name, ".", 1) && ft_strcmp(sd->d_name, ".."))
 		{
 			//store name to list;
 			tmp->content = sd->d_name;
 			tmp->content_size = ft_strlen(sd->d_name);
-			ft_printf("content: %s\nsize: %i\n", tmp->content, (int)tmp->content_size);
-			ft_lstadd(&name_list, tmp);
+			//ft_printf("content: %s\nsize: %i\n", tmp->content, (int)tmp->content_size);
+			ft_lstadd(name_list, tmp);
 		}
-
-
 	closedir(dir);
+	//print_list(name_list);
+
 }
 
 
@@ -45,7 +57,12 @@ int main(int ac, char **av)
 	name_list = NULL;
     if (ac == 1)
     {
- 		ls_openprintdir(name_list, ".");
+ 		ls_openprintdir(&name_list, ".");
+
+		ft_printf("%s", name_list->content);
+		exit(2);
+
+
 
 		//check system ls
 		ft_putendl("\n");
@@ -53,7 +70,7 @@ int main(int ac, char **av)
 
 		return (0);
     }
-	ls_openprintdir(name_list, av[1]);
+	ls_openprintdir(&name_list, av[1]);
 
 	//sort_str_array(ac, av);
     return (0);
