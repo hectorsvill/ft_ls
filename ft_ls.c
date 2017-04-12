f7af2e633e04bcc2eb2fc11a775bfaeeed020c55
hsv <hectorstevenvillasano@outlook.com>
2017-04-12 10:38:08 -0700

    im going with link list

--- ft_ls.c
+++ ft_ls.c
@@ -1,8 +1,13 @@
 #include "ft_ls.h"
 
+/*
+	1.store directory names in array or link list.
+	2.sort list of names with qsort.
+	3.print list of names
 
+*/
 
-void ls_openprintdir(char *name)
+void ls_openprintdir(t_list *name_list, char *name)
 {
     /**
     **  This fx writes a list of the directory name
@@ -10,36 +15,46 @@ void ls_openprintdir(char *name)
     DIR             *dir;
     struct dirent   *sd;
 
+	t_list			*tmp;
+
     dir = opendir(name);
     if (!dir)
-	{
 		ft_printf("ls: cannot access %s: No such file or directory\n", name);
-		exit(1);
-	}
-    else
-    {
-        while ((sd = readdir(dir)) != NULL)
-            if(ft_strncmp(sd->d_name, ".", 1) && ft_strcmp(sd->d_name, ".."))
-            {
-				ft_putendl(sd->d_name);
-				//ft_putstr("  ");
-			}
-    }
-    closedir(dir);
+
+	tmp = ft_lstnew("", 0);
+	name_list = ft_lstnew("", 0);
+	while ((sd = readdir(dir)) != NULL)
+		if(ft_strncmp(sd->d_name, ".", 1) && ft_strcmp(sd->d_name, ".."))
+		{
+			//store name to list;
+			tmp->content = sd->d_name;
+			tmp->content_size = ft_strlen(sd->d_name);
+			ft_printf("content: %s\nsize: %i\n", tmp->content, (int)tmp->content_size);
+			ft_lstadd(&name_list, tmp);
+		}
+
+
+	closedir(dir);
 }
 
 
 int main(int ac, char **av)
 {
+	t_list *name_list;
+
+	name_list = NULL;
     if (ac == 1)
     {
-      //  ls_openprintdir(".");
-		//ft_putchar('\n');
-		///system ("ls");
-        return (0);
+ 		ls_openprintdir(name_list, ".");
+
+		//check system ls
+		ft_putendl("\n");
+		system ("ls");
+
+		return (0);
     }
-    //ls_openprintdir(av[1]);
-    
-    sort_str_array(ac, av);
+	ls_openprintdir(name_list, av[1]);
+
+	//sort_str_array(ac, av);
     return (0);
 }
