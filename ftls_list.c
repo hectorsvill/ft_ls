580bfea235f3ae949e2cef8d29b49a5999fd0940
hsv <hectorstevenvillasano@outlook.com>
2017-04-13 19:22:33 -0700

    add ftls_list.c

---
+++ ftls_list.c
@@ -0,0 +1,28 @@
+void print_list(t_ftls *list)
+{
+	int sp;
+	sp = 0;
+	while (list)
+	{
+		if (ft_strncmp(list->names, ".", 1))
+			ft_putendl(list->names );
+		//ft_putstr("    ");
+		list = list->next;
+	}
+
+}
+
+void ftls_lstadd(t_ftls **alst, t_ftls *t_new)
+{
+	t_new->next = *alst;
+	*alst = t_new;
+}
+
+t_ftls *ftls_lstnew(char *name)
+{
+	t_ftls *link;
+
+	link = (t_ftls*)malloc(sizeof(t_ftls));
+	link->names = ft_strdup(name);
+	return (link);
+}
