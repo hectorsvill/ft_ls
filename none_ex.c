7364d0d95bd839eaeb7ec1ace97019541dc5bcc0
hsv <hectorstevenvillasano@outlook.com>
2017-04-14 17:22:13 -0700

    none_ex check for valid files

--- none_ex.c
+++ none_ex.c
@@ -5,15 +5,30 @@
 
 void addto_file_list(char **av, t_files **files)
 {
-	char ex;
-
-	ex = 0;
 	*files = NULL;
 	while (*av)
 	{
+		/**
+		**	check if *av is a valid flags
+		**	if valid flag then add too file_list
+		**	else add to none_ex;
+		**/
 		lstadd_files(files, *av);
-		//ft_putendl(*av);
 		av++;
 	}
+}
 
+
+char isvalidfile(t_files **dir_files, char *name)
+{
+	char valid;
+
+	valid = 0;
+	while(dir_files)
+	{
+		if (!strcmp(dir_files->file, name))
+			return (1);
+		dir_files = dir_files->next;
+	}
+	return (valid);
 }
